a2b152fb9b6fbad9b6c37df9a6e6f664b0ab8f94
Siarhei Bahamiaka <dexter0175@gmail.com>
2023-04-27 18:52:11 +0200

    Added formik

--- client/src/components/quiz.js
+++ client/src/components/quiz.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
+import { useFormik } from 'formik';
 
-const questions = [
+const questionsInitial = [
   {
     question: 'Что означает аббревиатура HTML?',
+    type: "SingleSelect",
     options: [
-      { text: 'a. HyperText Markup Language', isCorrect: true },
-      { text: 'b. HyperText Makeup Language', isCorrect: false },
-      { text: 'c. HyperText Model Language', isCorrect: true },
-    ]
+      { text: 'a. HyperText Markup Language', value: "mssmsm" },
+      { text: 'b. HyperText Makeup Language', value: "1"},
+      { text: 'c. HyperText Model Language',  value: "2" },
+    ],
+    correctAnswer: "mssmsm"
   },
+
   {
     question: 'Какой тег используется для создания заголовка на веб-странице?',
     options: [
@@ -29,17 +33,39 @@ const questions = [
 ];
 function Quiz() {
   const [score, setScore] = useState(0);
+  const [questions, setQuestions] = useState(questionsInitial.map(el => {
+    return {
+      ...el,
+      options: el.options.map( option => ({...option, selected: false}) )
+    }
+  }));
 
-  const handleAnswerOptionClick = (isCorrect, index) => {
+  // {0: "", 1: "",}
+  const form = useFormik({
+    initialValues: {}
+  });
+
+console.log(form.values);
+
+
+  const handleAnswerOptionClick = (option) => {
     const updatedQuestions = [...questions];
-    updatedQuestions[index].options.forEach((option) => {
-      option.selected = false;
-    });
-    updatedQuestions[index].options.find((option) => option.isCorrect === isCorrect).selected = true;
-    setScore(updatedQuestions.reduce((totalScore, question) => {
-      const selectedOption = question.options.find((option) => option.selected);
-      return selectedOption && selectedOption.isCorrect ? totalScore + 1 : totalScore;
-    }, 0));
+
+    option.selected = true;
+
+    setQuestions(updatedQuestions);
+
+
+    // updatedQuestions[index].options.forEach((option) => {
+    //   option.selected = false;
+    // });
+
+    // updatedQuestions[index].options.find((option) => option.isCorrect === isCorrect).selected = true;
+
+    // setScore(updatedQuestions.reduce((totalScore, question) => {
+    //   const selectedOption = question.options.find((option) => option.selected);
+    //   return selectedOption && selectedOption.isCorrect ? totalScore + 1 : totalScore;
+    // }, 0));
   };
 
   return (
@@ -50,6 +76,7 @@ function Quiz() {
 
       <div className='questions-section flex flex-col  '>
         {questions.map((question, index) => (
+
           <div className='question-section border rounded-md mx-2 my-4' key={index}>
             <div className='question-count'>
               <span>Вопрос {index + 1}</span>/{questions.length}
@@ -59,14 +86,18 @@ function Quiz() {
             <div className='answer-section flex flex-col justify-between mb-[0.5rem] min-h-[1.5rem] px-[1.5rem]'>
               {question.options.map((option, optionIndex) => (
                   <label key={optionIndex} className='answer-option'>
-                    <input type='radio' 
-                           onChange={() => handleAnswerOptionClick(option.isCorrect, index)} 
-                           checked={option.selected}
-                          />
+                    <input 
+                        type='radio' 
+                        name={index}
+                        value={option.value}
+                        onChange={form.handleChange}
+                    />
                     {option.text}
                   </label>
               ))}
             </div>
+{/* 
+           {if(question.type === "SelectOne") return <SelectOneAnswer /> } */}
           </div>
         ))}
       </div>
