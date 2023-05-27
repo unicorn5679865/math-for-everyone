815f5576243e44a3475d84847a88ccb7c138e165
Siarhei Bahamiaka <dexter0175@gmail.com>
2023-05-27 22:05:33 +0200

    refactoring

--- client/src/hooks/useQuery.js
+++ client/src/hooks/useQuery.js
@@ -1,37 +1,30 @@
 import { useErrorStatus } from '../components/ApiErrorHandler/ApiErrorHandler';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 const baseUrl = "http://localhost:5000/api";
 
-export const useQuery = (requestInfo, ...dependencies) => {
+export const useQuery = (url, dependencies=[]) => {
   const { setErrorStatusCode } = useErrorStatus();
   const [apiData, setApiData] = useState();
 
-  const [trigger, triggerRequest] = useState();
-  
-  let url, fetchOptions = {};
-  if (typeof requestInfo === 'string') {
-      url = `${baseUrl}${requestInfo}`;
-  } else {
-    fetchOptions = {
-      ...fetchOptions, url: `${baseUrl}${fetchOptions.url}`
-    }
-  }
-  
-  useEffect (() => {
-    fetch(url || fetchOptions, {credentials: 'include'})
-      .then(res => {
-        if (res.status >= 400) {
-          return setErrorStatusCode(res.status)
-        }
+  const fetchData = useCallback(() => {
+    return fetch(baseUrl + url, {credentials: 'include'})
+            .then(res => {
+              if (res.status >= 400) {
+                return setErrorStatusCode(res.status)
+              }
 
-        return res.json();
-      })
-      .then(apiData => {
-          setApiData(apiData); 
-        }
-      );
-  }, [requestInfo, trigger, ...dependencies]);
+              return res.json();
+            })
+            .then(apiData => {
+                setApiData(apiData); 
+              }
+            );
+  }, [url, ...dependencies])
+
+  useEffect (() => {
+    fetchData();
+  }, [fetchData]);
   
-  return {data: apiData, refresh: () => triggerRequest()};
+  return {data: apiData, refresh: fetchData};
 }
